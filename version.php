8b5ff222444680451bbdab1d6ff6fd53e6955171
Limekiller <me@bryceyoder.com>
2023-11-09 16:39:46 +0000

    Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_openai_chat';
-$plugin->version = 2023071400;
+$plugin->version = 2023110900;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.7.0';
