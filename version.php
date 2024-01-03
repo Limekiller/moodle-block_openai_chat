d4cb7859515361b84d506108f67e1b36916a6fb2
Limekiller <me@bryceyoder.com>
2024-01-03 13:32:04 -0500

    Bump supported Moodle

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_openai_chat';
 $plugin->version = 2024010300;
-$plugin->requires = 2020061500;
+$plugin->requires = 2022041600;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.0.0';
