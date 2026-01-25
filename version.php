29af9d455b0e9bbdb89ca2ef2fa00f677d6da682
Limekiller <me@bryceyoder.com>
2026-01-25 22:52:22 +0000

    Bump required version to 4.5

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_openai_chat';
 $plugin->version = 2026012501;
-$plugin->requires = 2022041600;
+$plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '4.0.0';
